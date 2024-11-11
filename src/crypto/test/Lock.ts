a0a87e538286b68fd3b6ef936444657599474f33
Valentin Kostuykovskiy <v.kostuykovskiy@gmail.com>
2024-11-11 21:14:26 +0100

    Project Structure Refactor

--- src/crypto/test/Lock.ts
+++ src/crypto/test/Lock.ts
@@ -78,7 +78,7 @@ describe("Lock", function () {
         await time.increaseTo(unlockTime);
 
         // We use lock.connect() to send a transaction from another account
-        await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
+        await expect(lock.connect(otherAccount)).to.be.revertedWith(
           "You aren't the owner"
         );
       });
